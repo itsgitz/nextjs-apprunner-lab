d239702e492ca0972453b50520e6343d784647eb
itsgitz <anggit.ginanjar.dev@gmail.com>
2025-12-09 10:15:30 +0700

    Nambah nama ALFA

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
                     priority
                 />
                 <div className="mb-10">
-                    <h1 className="text-3xl">App Runner</h1>
+                    <h1 className="text-3xl">App Runner ALFA!!!!!!!</h1>
                 </div>
                 <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
                     <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
